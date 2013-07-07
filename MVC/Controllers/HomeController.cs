64b0b7acca0671840f129e1cda0150ca862ae6a5
Rion <rionmonster@gmail.com>
2013-07-07 14:29:23 -0500

    Initial Commit

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MajorTom.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Example Index to render our Index View (that will handle all the communication)
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
